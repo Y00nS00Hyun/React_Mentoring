c9d8a0963169e54a88a80a799cedf7642cf1a4af
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-05-30 14:42:48 +0900

    어렵당

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
